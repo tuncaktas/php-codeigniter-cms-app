205c86c72bc85336da8bfa9f50356a1c63e7e725
tuncaktas <maktas027@gmail.com>
2023-07-14 10:43:42 +0300

    dashboard_v icin klasor olusturuldu ve bos content sayfasi yapildi

---
+++ panel/application/views/includes/footer.php
@@ -0,0 +1,10 @@
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+            <ul class="footer-menu pull-right">
+
+            </ul>
+            <div class="copyright pull-left">Tum haklari saklidir | TuncAktas <?php echo date("Y"); ?> &copy;</div>
+        </div>
+    </footer>
+</div>
